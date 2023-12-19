8221e18700eb9f121edd8cfc6657f5ca7a16fe63
Gishandamindu <gishankalasinghe1999@gmail.com>
2023-12-19 10:59:47 +0530

    update

--- src/Add.java
+++ src/Add.java
@@ -2,10 +2,46 @@ import java.util.Arrays;
 
 public class Add {
     public static void main(String[] args) {
-        int arr1[] = {3, 4};
-        int arr2[] = {8, 9};
+        int arr1[] = {3, 78}; // {8,9}
+        int arr2[] = {8, 9}; // {3,4}
 
-        // Print the contents of arr1
-        System.out.println(Arrays.toString(arr1));
+        // Create a new array with a length equal to the sum of the lengths of arr1 and arr2
+        int result[] = new int[arr1.length + arr2.length];
+        int j=0;
+        String status;
+        for (int i=0; i<2; i++){
+
+            if (arr1[i]<arr2[j+i]) {
+                result[i] = arr1[i];
+                status="false";
+            } else{
+                result[i]=arr2[i+j];
+                status="true";
+                //result[1]
+            }
+            j=j-1;
+        }
+
+        if (status=="false"){
+            result[2]=arr2[0];
+            result[3]=arr2[1];
+        }
+
+
+        for (int i=0;i<4;i++){
+            System.out.println(result[i]);
+
+        }
+        // Copy elements from arr1 to result
+        //System.arraycopy(arr1, 0, result, 0, arr1.length);
+
+        // Copy elements from arr2 to result, starting from the end of arr1
+        //System.arraycopy(arr2, 0, result, arr1.length, arr2.length);
+        // System.out.println(result[0]+result[1]+result[2]+result[3]);
+
+
+        // Print the result
+        //System.out.println(Arrays.toString(result));
     }
 }
+
