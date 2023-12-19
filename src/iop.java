b883f5de7570afc68a6d940f32212f55f2743be3
Gishandamindu <gishankalasinghe1999@gmail.com>
2023-12-20 00:44:29 +0530

    update

--- src/iop.java
+++ src/iop.java
@@ -1,41 +1,62 @@
 import java.util.Arrays;
 
 public class iop {
-    public static void main(String[] args) {
-        int arr1[] = {8, 19, 89}; // {8,9}
-        int arr2[] = {1,2,3,4,8,10,0}; // {3,4} {0,0,0,0,0,0,0}
-        int num1 = arr1.length + arr2.length;
-        int result[] = new int[arr1.length + arr2.length];
-        result[0]=arr2[0];
-        result[1]=arr2[1];
-        result[2]=arr2[2];
-        result[3]=arr2[3];
+    public static int[] insertNumbersortedArray(int[] arr2,int insertvalue){
         int num2 = arr2.length;
-        int h=7;
+        int h=insertvalue;
         for (int i=0;i<num2;i++){
-            if (h<arr2[i]){
-                int k=i+1;
-                for (int p=0;p<2;p++){
+            if (h<arr2[i] && i!=0){
+                int k=num2-2;
+                for (int p=0;p<num2-i-1;p++){
                     arr2[k+1]=arr2[k];
                     k=k-1;
                 }
                 arr2[i]=h;
                 break;
-
             }
-
-
-
-
-
-
-
-
+            else if(h<arr2[i]){
+                int k=num2-2;
+                for (int p=0;p<num2-i-1;p++){
+                    arr2[k+1]=arr2[k];
+                    k=k-1;
+                }
+                arr2[i]=h;
+                break;
+            }
         }
+        return arr2;
+
+    }
+    public static void main(String[] args) {
+        int arr2[] = {12,15,33,44,98,100,0}; // {3,4} {0,0,0,0,0,0,0}
+
+//        int num2 = arr2.length;
+//        int h=40;
+//        for (int i=0;i<num2;i++){
+//            if (h<arr2[i] && i!=0){
+//                int k=num2-2;
+//                for (int p=0;p<num2-i-1;p++){
+//                    arr2[k+1]=arr2[k];
+//                    k=k-1;
+//                }
+//                arr2[i]=h;
+//                break;
+//            }
+//            else if(h<arr2[i]){
+//                int k=num2-2;
+//                for (int p=0;p<num2-i-1;p++){
+//                    arr2[k+1]=arr2[k];
+//                    k=k-1;
+//                }
+//                arr2[i]=h;
+//                break;
+//            }
+//        }
         for (int i = 0; i < 7; i++) {
-            System.out.println(result[i]);
+            System.out.println(arr2[i]);
 
         }
+
 
 
 //
