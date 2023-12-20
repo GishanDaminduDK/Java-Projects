5b05df76725dc1573260666ce7b592833ed9eec9
Gishandamindu <gishankalasinghe1999@gmail.com>
2023-12-20 12:22:22 +0530

    update

--- src/deque.java
+++ src/deque.java
@@ -1,15 +1,39 @@
 import java.util.*;
+
 public class deque {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        Deque deque = new ArrayDeque<>();
-        int n = in.nextInt();
-        int m = in.nextInt();
+        Deque<Integer> deque = new ArrayDeque<>();
+        Set<Integer> uniqueSet = new HashSet<>();
 
-        for (int i = 0; i < n; i++) {
-            int num = in.nextInt();
+        int n = 6;//in.nextInt();
+        int m = 3;//in.nextInt();
+        int maxUniqueCount = 0;
+        //int[] numbers=new int[6];
+        int[] numbers={5,3,5,2,3,2};
 
+
+        for (int num:numbers) {
+            //int num = in.nextInt();
+
+            // Add the current element to the deque and set
+            deque.addLast(num);
+            uniqueSet.add(num);
+
+            // Maintain the size of the deque as 'm'
+            if (deque.size() > m) {
+                int removedElement = deque.removeFirst();
+                if (!deque.contains(removedElement)) {
+                    uniqueSet.remove(removedElement);
+                }
+            }
+
+            // Update the maximum unique count
+            if (deque.size() == m && uniqueSet.size() > maxUniqueCount) {
+                maxUniqueCount = uniqueSet.size();
+            }
         }
-    }
 
+        System.out.println(maxUniqueCount);
+    }
 }
